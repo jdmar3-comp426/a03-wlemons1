ca0a79ecfbd78371d7644beb844f238bb9940096
wlemons1 <wlemons1@live.unc.edu>
2021-11-18 14:49:27 -0500

    initial implementations of function currying sec

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -28,7 +28,9 @@ export const repeat = (fn, n, ...params) => {
  *   10 times.
  */
 export const repeatDemo = () => {
-
+    for(let i = 0; i < 10; i++) {
+        console.log("Hello, world!")
+    }
 };
 
 
@@ -45,7 +47,9 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-
+    return function(num2) {
+        return num1 * num2;
+    };
 };
 
 
@@ -53,7 +57,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -61,7 +65,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
